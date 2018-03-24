b3b32915925369144c9641716642906e7f4544e2
A MacGregor <annabel.macgregor@gmail.com>
2018-03-24 09:23:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,4 @@
-var playlist = new Object({artistName: songTitle})
+var playlist = {
+  artistName: songTitle
+};
 
-function updatePlaylist(obj, key, value) {
-  return Object.assign({}, { ${key}: ${value} });
-}
-
-function removeFromPlaylist(playlist, key) {
-  
-  return playlist;
-}
